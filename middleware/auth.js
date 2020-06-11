852b096c2e1abafed71283b726f1339ec80cea0c
Eightsh One <eightsh.one@gmail.com>
2020-06-11 22:50:58 +0100

    fix(auth): return role

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,6 +13,7 @@ const auth = async (req, res, next) => {
       throw new Error();
     }
     req.id = data._id;
+    req.role = data.role;
     req.token = token;
     next();
   } catch (error) {
