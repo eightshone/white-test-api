0abfd1ae8fe2477975f643ed945fba07e02a7d80
Eightsh One <eightsh.one@gmail.com>
2020-06-13 11:11:06 +0100

    feat(app+routers): init routers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userRouter = require("./routers/user");
+const classroomRouter = require("./routers/classroom");
+const whiteTestRouter = require("./routers/whiteTest");
 const port = process.env.PORT;
 require("./db/db")();
 
@@ -7,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(userRouter);
+app.use(classroomRouter);
+app.use(whiteTestRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
