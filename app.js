95dbd1314249c1340752a782af996c628b3c9f62
Eightsh One <eightsh.one@gmail.com>
2021-01-07 14:58:34 +0100

    fix(app): add cors support

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const userRouter = require("./routers/user");
 const classroomRouter = require("./routers/classroom");
 const whiteTestRouter = require("./routers/whiteTest");
+const cors = require('cors');
 const port = process.env.PORT;
 require("./db/db")();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(classroomRouter);
